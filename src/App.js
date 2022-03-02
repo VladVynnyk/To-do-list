3a2d5b5dac0fd9e50f299e468ab86ac9689f0edc
Vlad Vynnyk <vladvynnyk1@gmail.com>
2022-03-02 09:52:23 +0200

    Created header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>To do list</h1>
+        <Header/>
+        <div className="container">
+            <h1 style={{marginTop: 0}}>To do list</h1>
+        </div>
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './Header.module.css'
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <ul className={classes.navBar}>
+                <li>
+                    FirstItem
+                </li>
+                <li>
+                    SecondItem
+                </li>
+                <li>
+                    ThirdItem
+                </li>
+            </ul>
+            <h1 className={classes.logo}>To do list</h1>
+        </header>
+    );
+};
+
+export default Header;
